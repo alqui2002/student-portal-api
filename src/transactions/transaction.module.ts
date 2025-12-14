3a14c3228d0e20c4a79fea42d0ebdc8b9e7965dc
delfinapiendii <delfinapiendi@gmail.com>
2025-12-14 14:07:41 -0300

    added transaction module

---
+++ src/transactions/transaction.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { TransactionController } from './transaction.controller';
+import { TransactionService } from './transaction.service';
+import { Purchase } from '../purchases/entities/purchase.entity';
+import { Career } from '../career/entities/career.entity';
+
+@Module({
+    imports: [TypeOrmModule.forFeature([Purchase])],
+    controllers: [TransactionController],
+  providers: [TransactionService],
+})
+export class CoursesModule {}
