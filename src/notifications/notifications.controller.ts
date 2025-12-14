a9c29f04c1f571c1b61a557bf9e2376d63c8e544
delfinapiendii <delfinapiendi@gmail.com>
2025-12-14 15:18:52 -0300

    added notifications

--- src/notifications/notifications.controller.ts
+++ src/notifications/notifications.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Post, Body, Param, Patch, Query, UseGuards } from '@ne
 import { NotificationsService } from './notifications.service';
 import { CreateNotificationDto } from './dto/create-notification.dto';
 import { ExternalJwtAuthGuard } from 'src/auth/external-jwt.guard';
+import { CoreNotificationDto } from './dto/core-notification.dto';
 
 @Controller('/notifications')
 export class NotificationsController {
@@ -32,4 +33,10 @@ export class NotificationsController {
   ) {
     return this.notificationsService.update(id, body);
   }
+
+  @Post('core-event')
+  upsertFromCore(@Body() dto: CoreNotificationDto) {
+    return this.notificationsService.upsertFromCore(dto);
+  }
+
 }
