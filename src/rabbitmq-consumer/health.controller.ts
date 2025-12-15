6f4cfd52acd969531371cc98204dbce95a402059
delfinapiendii <delfinapiendi@gmail.com>
2025-12-15 16:12:26 -0300

    added health

---
+++ src/rabbitmq-consumer/health.controller.ts
@@ -0,0 +1,13 @@
+// health.controller.ts
+import { Controller, Get } from '@nestjs/common';
+
+@Controller('health')
+export class HealthController {
+  @Get()
+  ping() {
+    return {
+      status: 'ok',
+      timestamp: new Date().toISOString(),
+    };
+  }
+}
