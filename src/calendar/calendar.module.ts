d86531a3d57937053377a9065419496f5ee62378
Rodrigo Alcaraz <alcarazrodrigo1c@gmail.com>
2025-12-16 22:51:59 -0300

    first try of exams events

--- src/calendar/calendar.module.ts
+++ src/calendar/calendar.module.ts
@@ -4,10 +4,19 @@ import { CalendarController } from './calendar.controller';
 import { CalendarService } from './calendar.service';
 import { CalendarEvent } from './entities/calendar-event.entity';
 import { User } from '../user/entities/user.entity';
+import { HttpModule } from '@nestjs/axios';
+import { CalendarSyncService } from './calendar-sync.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CalendarEvent, User])],
+  imports: [
+    TypeOrmModule.forFeature([CalendarEvent]),
+    HttpModule,
+  ],
   controllers: [CalendarController],
-  providers: [CalendarService],
+  providers: [
+    CalendarService,
+    CalendarSyncService, // 👈 ESTO FALTABA
+  ],
 })
 export class CalendarModule {}
+
